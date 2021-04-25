c7c0db0733451f3d1dace7acef38457dc0b8e2ad
sebousan <sebousan@gmail.com>
2021-04-25 18:21:22 +0200

    add carousel

---
+++ src/js/commons/carousel.js
@@ -0,0 +1,24 @@
+/* global tns */
+import { tns } from './node_modules/tiny-slider/src/tiny-slider'
+
+window.jd.Carousel = {
+    init: function () {
+        'use strict';
+        var slider = null;
+        this.sliderElm = document.querySelectorAll('.js-carousel');
+        if (this.sliderElm === null) {
+            return;
+        }
+        slider = tns({
+            container: '.js-carousel',
+            items: 1,
+            slideBy: 'page',
+            autoplay: true
+        });
+    }
+};
+
+window.addEventListener('scroll', function () {
+    'use strict';
+    window.jd.Carousel.init();
+});
