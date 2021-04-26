55aa10346e1b407fa79d08f3c1c56c59574662fe
sebousan <sebousan@gmail.com>
2021-04-26 15:57:15 +0200

    carousel

--- src/js/commons/carousel.js
+++ src/js/commons/carousel.js
@@ -4,20 +4,28 @@
 window.stk.Carousel = {
     init: function () {
         'use strict';
-        this.sliderElm = document.querySelectorAll('.js-carousel');
+        this.sliderElm = document.querySelector('.js-carousel');
         if (this.sliderElm !== null) {
             this.set();
         }
     },
     set: function () {
         'use strict';
+        var controlsText = ['prev', 'next'];
+        if (this.sliderElm.dataset.prev) {
+            controlsText[0] = this.sliderElm.dataset.prev;
+        }
+        if (this.sliderElm.dataset.next) {
+            controlsText[1] = this.sliderElm.dataset.next;
+        }
         tns({
             autoplay: true,
             autoplayButtonOutput: false,
             container: '.js-carousel',
             items: 1,
             loop: false,
-            mouseDrag: true
+            mouseDrag: true,
+            controlsText: controlsText
         });
     }
 };
